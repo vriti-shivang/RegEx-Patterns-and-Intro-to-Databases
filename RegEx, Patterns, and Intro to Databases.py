e7916030ac72680b16e30b331f154452119ee587
vriti-shivang <63708886+vriti-shivang@users.noreply.github.com>
2020-04-25 09:40:07 +0530

    Add files via upload

---
+++ RegEx, Patterns, and Intro to Databases.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    N = int(input())
+    pattern = r"@gmail\.com$"
+    regex = re.compile(pattern)
+    firstNamesList = [] 
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+        firstName = firstNameEmailID[0]
+        emailID = firstNameEmailID[1]
+        if regex.search(emailID):
+            firstNamesList.append(firstName)
+    firstNamesList.sort()
+    for name in firstNamesList:
+        print(name)        
